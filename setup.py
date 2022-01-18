bd6d6277b70338bf6cbb57d844db37ffe84e5239
DillonB07 <dillonbarnes07@gmail.com>
2022-01-18 18:29:32 +0000

    Refactor to `pygitapi`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="pygitstats",
+    name="pygitapi",
     install_requires=[
         'requests'
     ],
